7200c01f3a68349284101ad0bc1aeed005c30cd3
user <user@DESKTOP-I8S3L67>
2023-03-15 13:46:12 +0900

    소수 구하기

---
+++ ConsoleApplication8.c
@@ -0,0 +1,21 @@
+﻿
+#include <stdio.h>
+
+#define MAX_NUM 100
+
+int main(void) {
+    int prime, i;
+    int total = 0;
+
+    for (prime = 2; prime <= MAX_NUM; prime++) {
+        for (i = 2; i < prime; i++) {
+            if (prime % i == 0)
+                break;
+        }
+        if (prime == i)
+            total += prime;
+    }
+
+    printf("0부터 100까지 소수들의 합:%d \n", total);
+
+}
